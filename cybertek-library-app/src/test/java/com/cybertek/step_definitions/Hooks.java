709bcae38931470e7ae272890f9545fb16b2e4be
emrahcinar <emrah-c@hotmail.com>
2020-10-27 12:57:20 -0400

    update

---
+++ cybertek-library-app/src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -0,0 +1,35 @@
+package com.cybertek.step_definitions;
+
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    private final static Logger logger=Logger.getLogger(Hooks.class);
+
+    @Before
+    public void setup(){
+        logger.info("::: Starting automation :::");
+        logger.info("Browser Type "+ ConfigurationReader.getProperty("browser"));
+        logger.info("URL: "+ConfigurationReader.getProperty("url"));
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+     if (scenario.isFailed()){
+         logger.info("Taking a screenshot");
+         byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+         scenario.attach(screenshot,"image/png",scenario.getName());
+
+     }
+     Driver.closeDriver();
+     logger.info("::: Ending automation :::");
+    }
+
+
+}
